176ccda5d3511a29d4eeee712cf7b5c002dba7d4
qwe345asd <vva.99@yandex.ru>
2019-06-30 03:11:24 +0300

    something aaaaaaaaaaaaa

--- get_user_repos_api.py
+++ get_user_repos_api.py
@@ -1,30 +1,46 @@
 from github import Github
 import time
+import pickle
 
-
-g = Github('70cf6b7f128508493dcdf31b929820875be8ed42')
+#g = Github('b50a226b8adb95c2b2f1793ca8c73cab5958adf7', per_page=100)
+g = Github(per_page=100)
 
 def get_user_stats(name):
-    try:
-        repos = [repo for repo in g.get_user(name).get_repos() if not repo.fork]
-        stars = sum(repo.stargazers_count for repo in repos)
-        topics = set(sum([repo.get_topics() for repo in repos], []))
-        langs = set(sum([list(repo.get_languages().keys()) for repo in repos], []))     
-    except:
-        time.sleep(10)
-
-    return (map(lambda x: x.url, repos), stars, topics, langs)
-
-
-if __name__ == "__main__":
+    print(f'Procesing user: {name}')
+    loop = True
+    while loop:
+        try:
+            repos = [repo for repo in g.get_user(name).get_repos() if not repo.fork]
+            stars = sum(repo.stargazers_count for repo in repos)
+            topics = set(sum([repo.get_topics() for repo in repos], []))
+            langs = set(sum([list(repo.get_languages().keys()) for repo in repos], []))     
     
-    stats = get_user_stats('mr8bit')
-    print('get repos')
-    for repo in stats[0]:
-        print(repo)
-    print('get stars')
-    print(stats[1])
-    print('get topics')
-    print(stats[2])
-    print('get langs')
-    print(stats[3])
+            loop = False
+            return (map(lambda x: x.url, repos), stars, topics, langs)
+
+        except Exception as e:
+            print(e)
+            loop = True
+            time.sleep(10)
+
+if __name__ == '__main__':
+    
+    with open('./data/python_users.txt') as myfile:
+        head = [next(myfile) for x in range(100)]
+
+    data = list(map(get_user_stats, head))
+
+    with open('./data/top_100_python.pickle', 'wb') as f:
+        pickle.dump(data, f)
+
+
+    #stats = get_user_stats('mr8bit')
+    #print('get repos')
+    #for repo in stats[0]:
+    #    print(repo)
+    #print('get stars')
+    #print(stats[1])
+    #print('get topics')
+    #print(stats[2])
+    #print('get langs')
+    #print(stats[3])
